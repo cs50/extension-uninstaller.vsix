18cce53836f00ee8d14ec0c43e0e6467cb1641d3
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-07-15 11:59:26 -0400

    added comments and example settings.json

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,22 @@
 import * as vscode from 'vscode';
 
+/**
+ * Uninstall extensions by their extension identifiers specified in settings.json.
+ *
+ * @param context vscode.ExtensionContext
+ */
 function uninstallExtension(context: vscode.ExtensionContext) {
+
+    // Get the extension name for current context
     const extName = context.extension.packageJSON['name'];
     try {
+
+        // Get the configuration for this extension
         vscode.workspace.getConfiguration(extName)['uninstall'].map((each: string) => {
+
+            // Remove matching extension
             if (vscode.extensions.getExtension(each) !== undefined) {
-                vscode.commands.executeCommand("workbench.extensions.uninstallExtension", each);
+                vscode.commands.executeCommand('workbench.extensions.uninstallExtension', each);
             }
         });
     } catch (e) {
@@ -15,8 +26,10 @@ function uninstallExtension(context: vscode.ExtensionContext) {
 
 export function activate(context: vscode.ExtensionContext) {
     uninstallExtension(context);
-	vscode.extensions.onDidChange(() => {uninstallExtension(context);});
-    vscode.workspace.onDidChangeConfiguration(() => {uninstallExtension(context);});
+	vscode.extensions.onDidChange(() => { uninstallExtension(context); });
+    vscode.workspace.onDidChangeConfiguration(() => { uninstallExtension(context); });
 }
 
-export function deactivate() {}
+export function deactivate() {
+    // Not used.
+}
